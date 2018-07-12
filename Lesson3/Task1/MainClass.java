4b4f2937d8d20eda2a3461ad067dcb46302ad5fa
Alexander <ka.ateist@gmail.com>
2018-07-12 14:15:42 +0300

    Сделал 1 задание, проверил

--- Lesson3/Task1/MainClass.java
+++ Lesson3/Task1/MainClass.java
@@ -1,7 +1,29 @@
-package Lesson3;
+package Lesson3.Task1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainClass {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        FileInputStream in = new FileInputStream("src/Lesson3/1.txt");
+        byte[] bytes = new byte[in.available()];
+        StringBuilder sb = new StringBuilder("");
+        in.read(bytes);
+        for (int i = 0; i < bytes.length; i++) {
+            sb.append((char)bytes[i]);
+        }
+        //System.out.println(new String(bytes));
+        printArrey(bytes);
+        in.close();
+    }
+
+
+    public static void printArrey(byte[] o){
+        ArrayList<Character> ch = new ArrayList<>();
+        for (int i = 0; i < o.length; i++) {
+            ch.add((char)o[i]);
+        }
+        System.out.println(ch);
     }
 }
