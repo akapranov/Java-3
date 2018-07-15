a981eacc645bf3b8003652abb22bb2757b19b3eb
Alexander <ka.ateist@gmail.com>
2018-07-15 15:39:17 +0300

    Аналогично первому написал метод для сканирования

--- Lesson4/Task3/MFU.java
+++ Lesson4/Task3/MFU.java
@@ -3,9 +3,10 @@ package Lesson4.Task3;
 public class MFU {
     Object printLock = new Object();
     Object scannLock = new Object();
+
     public void print(String doc, int n){
         synchronized (printLock){
-            System.out.printf("Begin: %s\n", doc);
+            System.out.printf("Begin printing: %s\n", doc);
             for (int i = 0; i < n; i++) {
                 try {
                     Thread.sleep(50);
@@ -14,7 +15,22 @@ public class MFU {
                 }
                 System.out.printf("Print page #%d\n", i);
             }
-            System.out.printf("End of %s\n", doc);
+            System.out.printf("End of printing %s\n", doc);
+        }
+    }
+
+    public void scann(String doc, int n){
+        synchronized (scannLock){
+            System.out.printf("Begin scanning: %s\n", doc);
+            for (int i = 0; i < n; i++) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("Scann page #%d\n", i);
+            }
+            System.out.printf("End of scanning %s\n", doc);
         }
     }
 }
