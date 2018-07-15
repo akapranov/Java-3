32f9a95e7bbfbe63a96fe0284aa4d389d48f0626
Alexander <ka.ateist@gmail.com>
2018-07-15 14:36:02 +0300

    создал класс doYourJob и конструктор к нему написал потоки, вызывающие данный клас поочереди

---
+++ Lesson4/Task1/MainClass.java
@@ -0,0 +1,29 @@
+package Lesson4.Task1;
+
+public class MainClass {
+    static volatile char str = 'A';
+
+    public static void main(String[] args) {
+        Object lock = new Object();
+
+        class doYourJob implements Runnable{
+            private char c;
+            private char nextC;
+            private int number = 5;
+
+            public doYourJob(char c, char nextC) {
+                this.c = c;
+                this.nextC = nextC;
+            }
+
+            @Override
+            public void run() {
+                
+            }
+        }
+
+    }
+    new Thread (new doYourJob('A', 'B')).start();
+    new Thread (new doYourJob('B', 'C')).start();
+    new Thread (new doYourJob('C', 'A')).start();
+}
