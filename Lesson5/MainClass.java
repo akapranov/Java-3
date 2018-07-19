cbbb64ecd29ce058930e7a425c59c5294000db27
Alexander <ka.ateist@gmail.com>
2018-07-19 13:39:07 +0300

    Добавил CountDownLatch

--- Lesson5/MainClass.java
+++ Lesson5/MainClass.java
@@ -1,19 +1,33 @@
 package Lesson5;
 
+import java.util.concurrent.CountDownLatch;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
+    public static final CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT);
+
     public static void main(String[] args) {
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
+
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        }
     }
 
 }
