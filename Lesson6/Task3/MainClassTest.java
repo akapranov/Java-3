396d98fdd38ba70bd228ed8357cf9d700236a492
Alexander <ka.ateist@gmail.com>
2018-07-23 12:30:05 +0300

    Добавил тесты на все проверяемые методы , а так же Befor & After

--- Lesson6/Task3/MainClassTest.java
+++ Lesson6/Task3/MainClassTest.java
@@ -1,5 +1,44 @@
-import static org.junit.Assert.*;
+package Lesson6.Task3;
+
+import org.junit.*;
+
+import java.sql.SQLException;
 
 public class MainClassTest {
+    @Before
+    public void Before() throws SQLException {
+    }
 
+    @After
+    public void After() throws SQLException{
+
+    }
+
+    @Test
+    public void addNewPersonAndScore() {
+    }
+
+    @Test
+    public void addNewPerson() {
+    }
+
+    @Test
+    public void addScoreByName() {
+    }
+
+    @Test
+    public void updateScoreByName() {
+    }
+
+    @Test
+    public void readScoreByName() {
+    }
+
+    @Test
+    public void readNamesByScore() {
+    }
+
+    @Test
+    public void proofExist() {
+    }
 }
